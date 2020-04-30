9ba1975dc17ac8c42fd1731a25c044bc752b8514
Baptiste Parsy <baptiste.parsy@epita.fr>
2020-04-30 15:10:26 +0200

    Set random color of codons with an hash

--- src/karyotes/codons/LoopCodon.cpp
+++ src/karyotes/codons/LoopCodon.cpp
@@ -60,9 +60,45 @@ action_func LoopCodon::repair(int cost, int threshold) {
     };
 }
 
+static sf::Color hsv(int hue, float sat, float val) {
+    hue %= 360;
+    while (hue < 0) hue += 360;
+
+    if (sat < 0.f) sat = 0.f;
+    if (sat > 1.f) sat = 1.f;
+
+    if (val < 0.f) val = 0.f;
+    if (val > 1.f) val = 1.f;
+
+    int h = hue / 60;
+    float f = float(hue) / 60 - (float)h;
+    float p = val * (1.f - sat);
+    float q = val * (1.f - sat * f);
+    float t = val * (1.f - sat * (1 - f));
+
+    switch (h) {
+        default:
+        case 0:
+        case 6:
+            return sf::Color(val * 255, t * 255, p * 255);
+        case 1:
+            return sf::Color(q * 255, val * 255, p * 255);
+        case 2:
+            return sf::Color(p * 255, val * 255, t * 255);
+        case 3:
+            return sf::Color(p * 255, q * 255, val * 255);
+        case 4:
+            return sf::Color(t * 255, p * 255, val * 255);
+        case 5:
+            return sf::Color(val * 255, p * 255, q * 255);
+    }
+}
+
 sf::CircleShape LoopCodon::circle(float radius) const {
     sf::CircleShape c(radius * vitality() / 5);
-    c.setFillColor(sf::Color::Blue);
+    static std::hash<const char*> hash;
+    auto hue = hash(reinterpret_cast<const char*>(&action_)) % 360;
+    c.setFillColor(hsv(hue, 1, 1));
     return c;
 }
 
