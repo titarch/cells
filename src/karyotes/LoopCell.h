29777675c1f3cc221d8a14f0852aa4596d43fd62
Baptiste Parsy <baptiste.parsy@epita.fr>
2020-04-30 10:50:58 +0200

    Reincrease eat reward of classic food

--- src/karyotes/LoopCell.h
+++ src/karyotes/LoopCell.h
@@ -80,7 +80,7 @@ public:
         LoopCell c(50, pos, 50);
         c.push_codons({
                               LoopCodon::hand_outwards(2),
-                              LoopCodon::eat(10),
+                              LoopCodon::eat(15),
                               LoopCodon::hand_inwards(2),
                               LoopCodon::locate_weak(3),
                               LoopCodon::repair(5, 10)
